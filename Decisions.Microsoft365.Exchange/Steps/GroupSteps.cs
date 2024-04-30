a1d8da8cfe3a28b0677ae0fba72abdcbcba56d3d
shawn.lirette <shawn.lirette@decisions.com>
2024-04-30 13:25:39 -0400

    [DT-040319] Microsoft Exchange Office 365 Module

--- Decisions.Microsoft365.Exchange/Steps/GroupSteps.cs
+++ Decisions.Microsoft365.Exchange/Steps/GroupSteps.cs
@@ -2,7 +2,9 @@ using System.Net.Http.Json;
 using System.Text;
 using Decisions.Microsoft365.Common;
 using Decisions.Microsoft365.Common.API.Group;
+using DecisionsFramework;
 using DecisionsFramework.Design.Flow;
+using DecisionsFramework.Design.Properties;
 using DecisionsFramework.ServiceLayer;
 using Newtonsoft.Json;
 
@@ -11,12 +13,13 @@ namespace Decisions.Microsoft365.Exchange.Steps
     [AutoRegisterMethodsOnClass(true, "Integration/Microsoft365/Exchange/Groups")]
     public class GroupSteps
     {
-        private static JsonSerializerSettings IgnoreNullValues = new()
+        private static readonly JsonSerializerSettings IgnoreNullValues = new()
         {
             NullValueHandling = NullValueHandling.Ignore
         };
         
-        public Microsoft365GroupList? ListGroups(ExchangeSettings? settingsOverride, bool filterUnified)
+        public Microsoft365GroupList? ListGroups(bool filterUnified,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride = null)
         {
             string urlExtension = (filterUnified) ? $"{Microsoft365UrlHelper.GetGroupUrl(null)}$filter=groupTypes/any(c:c+eq+'Unified')" : Microsoft365UrlHelper.GetGroupUrl(null);
             string result = GraphRest.Get(settingsOverride, urlExtension);
@@ -24,10 +27,19 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return JsonHelper<Microsoft365GroupList?>.JsonDeserialize(result);
         }
         
-        public Microsoft365Group? CreateGroup(ExchangeSettings? settingsOverride,
-            string? description, string displayName, string[]? groupTypes, bool mailEnabled, string mailNickname,
-            bool securityEnabled, string[]? ownerIds, string[]? memberIds)
+        public Microsoft365Group? CreateGroup(string? description, string displayName, string[]? groupTypes,
+            bool? mailEnabled, string mailNickname, bool? securityEnabled, string[]? ownerIds, string[]? memberIds,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
+            if (string.IsNullOrEmpty(mailNickname))
+            {
+                throw new BusinessRuleException("mailNickname cannot be null or empty. Please set a unique nickname.");
+            }
+            
+            mailEnabled ??= false;
+            securityEnabled ??= false;
+            groupTypes ??= Array.Empty<string>();
+            
             string[]? owners = (ownerIds != null) ? GetUserUrlStrings(ownerIds) : Array.Empty<string>();
             string[]? members = (memberIds != null) ? GetUserUrlStrings(memberIds) : Array.Empty<string>();
             
@@ -49,7 +61,8 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return JsonHelper<Microsoft365Group?>.JsonDeserialize(result);
         }
         
-        public Microsoft365Group? GetGroup(ExchangeSettings? settingsOverride, string groupId)
+        public Microsoft365Group? GetGroup(string groupId,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = Microsoft365UrlHelper.GetGroupUrl(groupId);
             string result = GraphRest.Get(settingsOverride, urlExtension);
@@ -57,18 +70,23 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return JsonHelper<Microsoft365Group?>.JsonDeserialize(result);
         }
         
-        public string UpdateGroup(ExchangeSettings? settingsOverride, string groupId, Microsoft365UpdateGroup group)
+        public string UpdateGroup(string groupId, Microsoft365UpdateGroup group,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = Microsoft365UrlHelper.GetGroupUrl(groupId);
+
+            // Some objects can only be patched individually, so separate requests will be made.
+            LesserGroupUpdates(settingsOverride, urlExtension, group);
             
-            HttpContent content = new StringContent(JsonConvert.SerializeObject(group, IgnoreNullValues), Encoding.UTF8,
-                "application/json");
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(group, IgnoreNullValues),
+                Encoding.UTF8, "application/json");
             HttpResponseMessage response = GraphRest.HttpResponsePatch(settingsOverride, urlExtension, content);
             
             return response.StatusCode.ToString();
         }
         
-        public string DeleteGroup(ExchangeSettings? settingsOverride, string groupId)
+        public string DeleteGroup(string groupId,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = Microsoft365UrlHelper.GetGroupUrl(groupId);
             HttpResponseMessage response = GraphRest.Delete(settingsOverride, urlExtension);
@@ -76,7 +94,8 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return response.StatusCode.ToString();
         }
         
-        public Microsoft365MemberList? ListMembers(ExchangeSettings? settingsOverride, string groupId)
+        public Microsoft365MemberList? ListMembers(string groupId,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = $"{Microsoft365UrlHelper.GetGroupUrl(groupId)}/members";
             string result = GraphRest.Get(settingsOverride, urlExtension);
@@ -84,7 +103,8 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return JsonHelper<Microsoft365MemberList?>.JsonDeserialize(result);
         }
         
-        public string AddMembers(ExchangeSettings? settingsOverride, string groupId, string[] directoryObjectIds)
+        public string AddMembers(string groupId, string[] directoryObjectIds,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = Microsoft365UrlHelper.GetGroupUrl(groupId);
 
@@ -105,7 +125,8 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return response.StatusCode.ToString();
         }
         
-        public string RemoveMember(ExchangeSettings? settingsOverride, string groupId, string directoryObjectId)
+        public string RemoveMember(string groupId, string directoryObjectId,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = $"{Microsoft365UrlHelper.GetGroupUrl(groupId)}/members/{directoryObjectId}/$ref";
             HttpResponseMessage response = GraphRest.Delete(settingsOverride, urlExtension);
@@ -113,7 +134,8 @@ namespace Decisions.Microsoft365.Exchange.Steps
             return response.StatusCode.ToString();
         }
         
-        public Microsoft365GroupCollection? ListMemberOf(ExchangeSettings? settingsOverride, string userIdentifier)
+        public Microsoft365GroupCollection? ListMemberOf(string userIdentifier,
+            [PropertyClassification(0, "Settings Override", "Settings")] ExchangeSettings? settingsOverride)
         {
             string urlExtension = $"{Microsoft365UrlHelper.GetUserUrl(userIdentifier)}/memberOf";
             string result = GraphRest.Get(settingsOverride, urlExtension);
@@ -130,6 +152,26 @@ namespace Decisions.Microsoft365.Exchange.Steps
             }
 
             return userUrls.ToArray();
+        }
+
+        private void LesserGroupUpdates(ExchangeSettings? settingsOverride, string urlExtension, Microsoft365UpdateGroup group)
+        {
+            List<Microsoft365UpdateGroup> lesserGroups = new List<Microsoft365UpdateGroup>();
+            if (group.AllowExternalSenders != null)
+            {
+                lesserGroups.Add(new() { AllowExternalSenders = group.AllowExternalSenders });
+            }
+            if (group.AutoSubscribeNewMembers != null)
+            {
+                lesserGroups.Add(new() { AutoSubscribeNewMembers = group.AutoSubscribeNewMembers});
+            }
+
+            foreach (Microsoft365UpdateGroup lesserGroup in lesserGroups)
+            {
+                HttpContent content = new StringContent(JsonConvert.SerializeObject(lesserGroup, IgnoreNullValues),
+                    Encoding.UTF8, "application/json");
+                GraphRest.HttpResponsePatch(settingsOverride, urlExtension, content);
+            }
         }
     }
 }
