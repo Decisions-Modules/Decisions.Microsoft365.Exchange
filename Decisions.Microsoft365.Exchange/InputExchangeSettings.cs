b5af774e80195a5c1e57a6f69f3995aa42b36e2e
Sami <sami.uddin@decisions.com>
2024-07-22 10:43:19 -0400

    DT-041410: Hiding createChildEntityInFolderId from dialog

--- Decisions.Microsoft365.Exchange/InputExchangeSettings.cs
+++ Decisions.Microsoft365.Exchange/InputExchangeSettings.cs
@@ -44,6 +44,7 @@ public class InputExchangeSettings: ICreateChildEntityInFolder
 
     private string createChildEntityInFolderId;
 
+    [PropertyHidden]
     public string CreateChildEntityInFolderId
     {
         get => createChildEntityInFolderId;
