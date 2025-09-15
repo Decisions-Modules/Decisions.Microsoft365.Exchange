57ea0dda6ee554577f9494d3a8bffbec7d34d6f0
Scott Roybal <scott.roybal@decisions.com>
2025-09-15 09:22:49 -0400

    [DT-045722] Microsoft 365 Exchange module: add Attachments input to Send Email step
     Added attachment input for Send Email step

--- Decisions.Microsoft365.Exchange/Steps/ExchangeEmailSteps.cs
+++ Decisions.Microsoft365.Exchange/Steps/ExchangeEmailSteps.cs
@@ -2,6 +2,7 @@ using System.Net.Http.Json;
 using Decisions.Microsoft365.Common;
 using Decisions.Microsoft365.Common.API.Email;
 using DecisionsFramework;
+using DecisionsFramework.Data.DataTypes;
 using DecisionsFramework.Design.Flow;
 using DecisionsFramework.Design.Properties;
 
@@ -126,14 +127,25 @@ public class ExchangeEmailSteps
             return response.StatusCode.ToString();
         }
         
+        [ExcludeMethodOnAutoRegister]
         public string SendEmail(string userIdentifier, string[] to, string[]? cc, string subject, string? body,
             Microsoft365BodyType? contentType, bool saveToSentItems,
-            [PropertyClassification(0, "Settings Override", "Settings")] InputExchangeSettings? settingsOverride)
+            [PropertyClassification(0, "Settings Override", "Settings")]
+            InputExchangeSettings? settingsOverride)
+        {
+            return SendEmailWithAttachments(userIdentifier, to, cc, subject, body, contentType, saveToSentItems, settingsOverride, null);
+        }
+        
+        [AutoRegisterMethod("Send Email")]
+        public string SendEmailWithAttachments(string userIdentifier, string[] to, string[]? cc, string subject, string? body,
+            Microsoft365BodyType? contentType, bool saveToSentItems,
+            [PropertyClassification(0, "Settings Override", "Settings")] InputExchangeSettings? settingsOverride, FileData[]? attachments)
         {
             string urlExtension = $"{Microsoft365UrlHelper.GetUserUrl(userIdentifier)}/sendMail";
             
             Microsoft365Recipient[] recipients = GetRecipients(to) ?? Array.Empty<Microsoft365Recipient>();
             Microsoft365Recipient[]? ccRecipients = (cc != null) ? GetRecipients(cc) : Array.Empty<Microsoft365Recipient>();
+            Microsoft365Attachment[] messageAttachments = attachments != null ? CreateAttachments(attachments) : Array.Empty<Microsoft365Attachment>();
 
             Microsoft365SendEmailRequest emailMessage = new()
             {
@@ -146,9 +158,10 @@ public class ExchangeEmailSteps
                     },
                     Subject = subject,
                     ToRecipients = recipients,
-                    CcRecipients = ccRecipients
+                    CcRecipients = ccRecipients,
+                    Attachments = messageAttachments
                 },
-                SaveToSentItems = saveToSentItems
+                SaveToSentItems = saveToSentItems,
             };
             
             JsonContent content = JsonContent.Create(emailMessage);
@@ -263,5 +276,25 @@ public class ExchangeEmailSteps
             });
 
             return recipients.ToArray();
+        }
+
+        private static Microsoft365Attachment[] CreateAttachments(FileData[]? files)
+        {
+            List<Microsoft365Attachment> attachments = new();
+
+            if (files is { Length: > 0 })
+            {
+                foreach (FileData file in files)
+                {
+                    attachments.Add(new Microsoft365Attachment
+                    {
+                        ODataType = "#microsoft.graph.fileAttachment",
+                        Name = file.FileName,
+                        ContentBytes = Convert.ToBase64String(file.Contents),
+                    });
+                }
+            }
+            
+            return attachments.ToArray();
         }
 }
